9867fce716fdd24569cb2a47c73be2cb00a9c5f7
hoanganhcndl-cmd <hoanganhcndl@gmail.com>
2026-04-05 17:39:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ from io import StringIO
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import asyncio
 import threading
+import asyncio
 
 # ===========================
-# CONFIG CSV
+# CONFIG
 # ===========================
 BUY_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6Xwxi0HpFNQWZiXg72eJfa2b1kaU3r2Be7B1I_hjj42k0NkAKJe0W3vM56KewYW52bkUIFLsvbn66/pub?gid=0&single=true&output=csv"
 SELL_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6Xwxi0HpFNQWZiXg72eJfa2b1kaU3r2Be7B1I_hjj42k0NkAKJe0W3vM56KewYW52bkUIFLsvbn66/pub?gid=968456620&single=true&output=csv"
@@ -67,12 +67,7 @@ def home():
 # RUN TELEGRAM BOT ASYNC
 # ===========================
 async def run_telegram_bot():
-    # Token demo
     TOKEN = os.getenv("BOT_TOKEN", "8746767158:AAGnKeB3S4zHZO0oZdEex8SLLX7JstDlSTs")
-    if not TOKEN:
-        print("❌ Thiếu BOT_TOKEN")
-        return
-
     print("🚀 Starting Telegram Bot...")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
@@ -87,7 +82,7 @@ async def run_telegram_bot():
 # ENTRY POINT
 # ===========================
 if __name__ == "__main__":
-    # chạy Flask server trong thread riêng
+    # Flask server trong thread riêng
     threading.Thread(
         target=lambda: server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000))),
         daemon=True
